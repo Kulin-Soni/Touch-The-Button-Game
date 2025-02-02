995e1dab389e85d2a08b27bffa06f6c2eb407cc8
Kulin Soni <93784469+Kulin-Soni@users.noreply.github.com>
2025-02-02 11:03:02 +0530

    Made the code more managable.

--- main.js
+++ main.js
@@ -2,22 +2,21 @@ const hovOb = document.getElementById("objectj"),
   locateE = document.getElementById("location");
 let xPo;
 let yPo;
-xPo = Math.floor(Math.random() * ((document.querySelector("#box").offsetWidth - hovOb.offsetWidth) + 1) + 1);
-yPo = Math.floor(Math.random() * ((document.querySelector("#box").offsetHeight - hovOb.offsetHeight) + 1) + 1);
-hovOb.style.top = `${yPo}px`;
-hovOb.style.left = `${xPo}px`;
-
-let r = setInterval(() => {
+alert("Touch the white button if you can!")
+const moveButton = ()=>{
   xPo = Math.floor(Math.random() * ((document.querySelector("#box").offsetWidth - hovOb.offsetWidth) + 1) + 1);
   yPo = Math.floor(Math.random() * ((document.querySelector("#box").offsetHeight - hovOb.offsetHeight) + 1) + 1);
   hovOb.style.top = `${yPo}px`;
   hovOb.style.left = `${xPo}px`;
+}
+let r = setInterval(() => {
+ moveButton()
 }, 100)
 
 hovOb.addEventListener("click", () => {
   clearInterval(r);
   hovOb.style.cssText = "background: #33FF06; box-shadow: 0 0 50px 0 #33FF06;";
-  locateE.innerHTML = `Congratulations! <a href="/">Reload To Try Again</a>`;
+  alert("Reload the website to try again");
   hovOb.style.top = `${yPo}px`;
   hovOb.style.left = `${xPo}px`;
 })
